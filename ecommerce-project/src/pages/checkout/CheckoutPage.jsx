011f8917f73aadc08d09959f96b70f3667dee283
thisHan <reyhanhariady1@gmail.com>
2026-07-27 17:11:10 +0700

    Create update delivery option feature

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -5,20 +5,28 @@ import { CheckoutHeader } from './CheckoutHeader'
 import { OrderSummary } from './OrderSummary'
 import { PaymentSummary } from './PaymentSummary'
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
-  useEffect(() => {
-    const getCheckoutData = async () => {
-      let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
-      setDeliveryOptions(response.data);
+  const getDeliveryOptionsData = async () => {
+    let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
+    setDeliveryOptions(response.data);
+  }
 
-      response = await axios.get('/api/payment-summary');
-      setPaymentSummary(response.data);
-    }
-    getCheckoutData();
+  const getPaymentSummaryData = async () => {
+    const response = await axios.get('/api/payment-summary');
+    setPaymentSummary(response.data);
+  }
+
+  useEffect(() => {
+
+    getDeliveryOptionsData();
   }, []);
+  useEffect(() => {
+
+    getPaymentSummaryData();
+  }, [cart]);
 
   return (
     <>
@@ -30,7 +38,7 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
       </div>
