8546f4fb9adfb224a98b0d22baf6cfac213a2877
thisHan <reyhanhariady1@gmail.com>
2026-07-29 21:24:14 +0700

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach, beforeAll } from "vitest";
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import axios from 'axios';
@@ -7,8 +7,15 @@ import { Product } from "./Product";
 vi.mock('axios');// create a mock for axios package
 
 describe('Product Component', () => {
-  it('display product detail correctly', () => {
-    const product = {
+
+  let product;
+
+  let loadCart;//vi.fn() ==>> buat mocking, ini cuma function kosong, ga ngelakuin apa-apa
+
+  let addToCart;
+
+  beforeEach(() => {
+    product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,7 +27,18 @@ describe('Product Component', () => {
       keywords: ["socks", "sports", "apparel"]
     };
 
-    const addToCart = vi.fn()//vi.fn() ==>> buat mocking, ini cuma function kosong, ga ngelakuin apa-apa
+    loadCart = vi.fn();
+
+    addToCart = async (productId, quantity) => {
+      await axios.post('/api/cart-items', {
+        productId: productId,
+        quantity: quantity || 1
+      });
+      await loadCart()
+    }
+  })
+
+  it('display product detail correctly', () => {
 
     render(<Product product={product} addToCart={addToCart} />)
     //render buat munculin component nya
@@ -30,7 +48,7 @@ describe('Product Component', () => {
 
     expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
     //toBeInTheDocument provided by @testing-library/jest-dom
-    expect(screen.getByTestId('product-rating-stars')).toHaveAttribute('src', `images/ratings/rating-${product.rating.stars * 10}.png`)
+    expect(screen.getByTestId('product-rating-stars')).toHaveAttribute('src', `images/ratings/rating-45.png`)
     expect(screen.getByText('87')).toBeInTheDocument();
 
     expect(screen.getByText('$10.90')).toBeInTheDocument();
@@ -38,27 +56,8 @@ describe('Product Component', () => {
   });
 
   it('adds a product to the cart', async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"]
-    };
 
-    const loadCart = vi.fn()//vi.fn() ==>> buat mocking, ini cuma function kosong, ga ngelakuin apa-apa
-    
-    const addToCart = async (productId, quantity) => {
-      await axios.post('/api/cart-items', {
-        productId: productId,
-        quantity: quantity || 1
-      });
-      await loadCart()
-    }
+
 
     render(<Product product={product} addToCart={addToCart} />)
 
